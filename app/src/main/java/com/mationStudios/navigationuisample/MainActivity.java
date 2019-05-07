38fc8b4a0fbf9c9ddf2de110ab6e7c2ab9b7cc18
freedom <freedom.chuks7@gmail.com>
2019-05-07 06:25:11 -0700

    added navigation ui depndency and added navigation components

--- app/src/main/java/com/mationStudios/navigationuisample/MainActivity.java
+++ app/src/main/java/com/mationStudios/navigationuisample/MainActivity.java
@@ -1,14 +1,26 @@
 package com.mationStudios.navigationuisample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpNavigation();
+    }
+    public void setUpNavigation(){
+        bottomNavigationView =findViewById(R.id.bttm_nav);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,
+                navHostFragment.getNavController());
     }
 }
