261d2c1ec48421388e38868840a196bd7e61d731
freedom <freedom.chuks7@gmail.com>
2019-05-07 05:14:32 -0700

    initial update

---
+++ app/src/main/java/com/mationStudios/navigationuisample/Fragments/Explore.java
@@ -0,0 +1,32 @@
+package com.mationStudios.navigationuisample.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mationStudios.navigationuisample.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Explore extends Fragment {
+
+
+    public Explore() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/mationStudios/navigationuisample/Fragments/Home.java
@@ -0,0 +1,32 @@
+package com.mationStudios.navigationuisample.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mationStudios.navigationuisample.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Home extends Fragment {
+
+
+    public Home() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
